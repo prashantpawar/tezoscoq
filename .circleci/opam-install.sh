1dc963eeb93d8590d835a7035610b7ffd227de70
Prashant Singh Pawar <prashantpawar@gmail.com>
2018-06-02 23:49:20 -0400

    - updated config.yml and opam-install.sh

---
+++ .circleci/opam-install.sh
@@ -0,0 +1,7 @@
+set -x
+set -e
+if [ ! -e opam-full-2.0.0-rc2 ]; then
+  wget https://github.com/ocaml/opam/releases/download/2.0.0-rc2/opam-full-2.0.0-rc2.tar.gz
+  tar -zxvf opam-full-2.0.0-rc2.tar.gz
+  cd opam-full-2.0.0-rc2 && ./configure && make
+fi
