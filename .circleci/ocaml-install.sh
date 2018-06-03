7a4d5e75b30bfba8b17a5dde4d4b8a28aa32f3d6
Prashant Singh Pawar <prashantpawar@gmail.com>
2018-06-03 01:50:16 -0400

    - Added steps for ocmal installation

---
+++ .circleci/ocaml-install.sh
@@ -0,0 +1,9 @@
+set -x
+set -e
+echo "Installing Ocaml`"
+if [ ! -e ocaml-4.06.0 ]; then
+  wget http://caml.inria.fr/pub/distrib/ocaml-4.06/ocaml-4.06.0.tar.gz
+  tar -zxvf ocaml-4.06.0.tar.gz
+  cd ocaml-4.06.0 && ./configure && make
+  sudo make install
+fi
