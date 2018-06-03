0c99d3bbf1035870151cb6c00448d86d7589bfb2
Prashant Singh Pawar <prashantpawar@gmail.com>
2018-06-03 01:52:20 -0400

    - added proper steps for ocaml build

--- .circleci/ocaml-install.sh
+++ .circleci/ocaml-install.sh
@@ -4,6 +4,6 @@ echo "Installing Ocaml"
 if [ ! -e ocaml-4.06.0 ]; then
   wget http://caml.inria.fr/pub/distrib/ocaml-4.06/ocaml-4.06.0.tar.gz
   tar -zxvf ocaml-4.06.0.tar.gz
-  cd ocaml-4.06.0 && ./configure && make
+  cd ocaml-4.06.0 && ./configure && make world.opt
   sudo make install
 fi
